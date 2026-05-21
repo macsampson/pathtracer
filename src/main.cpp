e5fae9a4719e0e5e85ece489abeeb1794e461083
Mackenzie Sampson <mackenzie.sampson@outlook.com>
2026-05-21 22:47:26 +0000

    add scenes

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,10 @@ void light_testing(int image_width, int samples_per_pixel, int max_depth, bool s
 void cornell_box(int image_width, int samples_per_pixel, int max_depth, bool single_thread);
 void cornell_random_spheres(int image_width, int samples_per_pixel, int max_depth, bool single_thread);
 void cornell_variety(int image_width, int samples_per_pixel, int max_depth, bool single_thread);
-void obj_mesh(int image_width, int samples_per_pixel, int max_depth, bool single_thread);
+void majora_mesh(int image_width, int samples_per_pixel, int max_depth, bool single_thread);
+void dragon_mesh(int image_width, int samples_per_pixel, int max_depth, bool single_thread);
+void armadillo_mesh(int image_width, int samples_per_pixel, int max_depth, bool single_thread);
+void suzanne_mesh(int image_width, int samples_per_pixel, int max_depth, bool single_thread);
 // TODO: make a scene with a glass cube full of metal spheres.
 
 struct Scene {
@@ -49,7 +52,10 @@ int main(int argc, char* argv[]) {
 		{"cornell_box", cornell_box},
 		{"cornell_random", cornell_random_spheres},
 		{"cornell_variety", cornell_variety},
-		{"obj_mesh", obj_mesh},
+		{"majora_mesh", majora_mesh},
+		{"dragon_mesh", dragon_mesh},
+		{"armadillo_mesh", armadillo_mesh},
+		{"suzanne_mesh", suzanne_mesh},
 	};
 
 	// Defaults
@@ -781,7 +787,7 @@ void cube_room(int image_width, int samples_per_pixel, int max_depth, bool singl
 
 // Renders an OBJ file inside a Cornell box. Place your OBJ at assets/model.obj.
 // load_obj_fit auto-scales to fit; adjust the translate to reposition as needed.
-void obj_mesh(int image_width, int samples_per_pixel, int max_depth, bool single_thread) {
+void majora_mesh(int image_width, int samples_per_pixel, int max_depth, bool single_thread) {
 	hittable_list world;
 
 	auto red = make_shared<lambertian>(color(.65, .05, .05));
@@ -809,11 +815,165 @@ void obj_mesh(int image_width, int samples_per_pixel, int max_depth, bool single
 	auto mesh_mat = make_shared<lambertian>(color(0.8, 0.7, 0.6));
 	auto glass_mat = make_shared<dielectric>(1.5);
 	auto red_metal = make_shared<metal>(color(.65, .05, .05), 0);
-	auto obj = load_obj_fit("assets/midna.obj", mesh_mat, 700.0);
+	auto obj = load_obj_fit("assets/majora/MajorasMask.obj", mesh_mat, 500.0, true, glass_mat);
 	// obj = make_shared<rotate_x>(obj, -90);
 	obj = make_shared<rotate_y>(obj, 180);
 	// obj = make_shared<rotate_z>(obj, 45);
 	obj = make_shared<translate>(obj, vec3(278, 0, 278));
+	world.add(obj);
+
+	world = hittable_list(make_shared<bvh_node>(world));
+
+	camera cam;
+	cam.aspect_ratio = 1.0;
+	cam.image_width = image_width;
+	cam.samples_per_pixel = samples_per_pixel;
+	cam.max_depth = max_depth;
+	cam.background = color(0, 0, 0);
+	cam.vfov = 40;
+	cam.lookfrom = point3(278, 278, -800);
+	cam.lookat = point3(278, 278, 0);
+	cam.vup = vec3(0, 1, 0);
+	cam.defocus_angle = 0;
+	cam.single_thread = single_thread;
+	cam.render(world, lights);
+}
+
+void dragon_mesh(int image_width, int samples_per_pixel, int max_depth, bool single_thread) {
+	hittable_list world;
+
+	auto red = make_shared<lambertian>(color(.65, .05, .05));
+	auto white = make_shared<lambertian>(color(.73, .73, .73));
+	auto green = make_shared<lambertian>(color(.12, .45, .15));
+	auto light = make_shared<diffuse_light>(color(15, 15, 15));
+
+	world.add(make_shared<quad>(point3(555, 0, 0), vec3(0, 555, 0), vec3(0, 0, 555), red));
+	world.add(make_shared<quad>(point3(0, 0, 0), vec3(0, 555, 0), vec3(0, 0, 555), green));
+	auto light_quad = make_shared<quad>(point3(343, 554, 332), vec3(-130, 0, 0), vec3(0, 0, -105), light);
+	world.add(light_quad);
+	world.add(make_shared<quad>(point3(0, 0, 0), vec3(555, 0, 0), vec3(0, 0, 555), white));
+	world.add(make_shared<quad>(point3(555, 555, 555), vec3(-555, 0, 0), vec3(0, 0, -555), white));
+	world.add(make_shared<quad>(point3(0, 0, 555), vec3(555, 0, 0), vec3(0, 555, 0), white));
+
+	shared_ptr<hittable> light_sphere
+		= make_shared<sphere>(point3(0, 0, 0), 35, make_shared<diffuse_light>(color(15, 15, 15)));
+	light_sphere = make_shared<translate>(light_sphere, vec3(470, 75, 150));
+	world.add(light_sphere);
+
+	hittable_list lights;
+	lights.add(light_quad);
+	lights.add(light_sphere);
+
+	auto mesh_mat = make_shared<lambertian>(color(0.8, 0.7, 0.6));
+	auto glass_mat = make_shared<dielectric>(1.5);
+	auto red_metal = make_shared<metal>(color(.65, .05, .05), 0);
+	auto obj = load_obj_fit("assets/dragon.obj", mesh_mat, 500.0, true, glass_mat);
+	obj = make_shared<rotate_x>(obj, -90);
+	obj = make_shared<rotate_y>(obj, 200);
+	// obj = make_shared<rotate_z>(obj, 45);
+	obj = make_shared<translate>(obj, vec3(278, 278, 278));
+	world.add(obj);
+
+	world = hittable_list(make_shared<bvh_node>(world));
+
+	camera cam;
+	cam.aspect_ratio = 1.0;
+	cam.image_width = image_width;
+	cam.samples_per_pixel = samples_per_pixel;
+	cam.max_depth = max_depth;
+	cam.background = color(0, 0, 0);
+	cam.vfov = 40;
+	cam.lookfrom = point3(278, 278, -800);
+	cam.lookat = point3(278, 278, 0);
+	cam.vup = vec3(0, 1, 0);
+	cam.defocus_angle = 0;
+	cam.single_thread = single_thread;
+	cam.render(world, lights);
+}
+void armadillo_mesh(int image_width, int samples_per_pixel, int max_depth, bool single_thread) {
+	hittable_list world;
+
+	auto red = make_shared<lambertian>(color(.65, .05, .05));
+	auto white = make_shared<lambertian>(color(.73, .73, .73));
+	auto green = make_shared<lambertian>(color(.12, .45, .15));
+	auto light = make_shared<diffuse_light>(color(15, 15, 15));
+
+	world.add(make_shared<quad>(point3(555, 0, 0), vec3(0, 555, 0), vec3(0, 0, 555), red));
+	world.add(make_shared<quad>(point3(0, 0, 0), vec3(0, 555, 0), vec3(0, 0, 555), green));
+	auto light_quad = make_shared<quad>(point3(343, 554, 332), vec3(-130, 0, 0), vec3(0, 0, -105), light);
+	world.add(light_quad);
+	world.add(make_shared<quad>(point3(0, 0, 0), vec3(555, 0, 0), vec3(0, 0, 555), white));
+	world.add(make_shared<quad>(point3(555, 555, 555), vec3(-555, 0, 0), vec3(0, 0, -555), white));
+	world.add(make_shared<quad>(point3(0, 0, 555), vec3(555, 0, 0), vec3(0, 555, 0), white));
+
+	shared_ptr<hittable> light_sphere
+		= make_shared<sphere>(point3(0, 0, 0), 35, make_shared<diffuse_light>(color(15, 15, 15)));
+	light_sphere = make_shared<translate>(light_sphere, vec3(470, 75, 150));
+	world.add(light_sphere);
+
+	hittable_list lights;
+	lights.add(light_quad);
+	lights.add(light_sphere);
+
+	auto mesh_mat = make_shared<lambertian>(color(0.8, 0.7, 0.6));
+	auto glass_mat = make_shared<dielectric>(1.5);
+	auto purple_metal = make_shared<metal>(color(.65, .05, .65), 0);
+	auto obj = load_obj_fit("assets/armadillo.obj", mesh_mat, 500.0, true, purple_metal);
+	// obj = make_shared<rotate_x>(obj, -90);
+	// obj = make_shared<rotate_y>(obj, 200);
+	// obj = make_shared<rotate_z>(obj, 45);
+	obj = make_shared<translate>(obj, vec3(278, 170, 278));
+	world.add(obj);
+
+	world = hittable_list(make_shared<bvh_node>(world));
+
+	camera cam;
+	cam.aspect_ratio = 1.0;
+	cam.image_width = image_width;
+	cam.samples_per_pixel = samples_per_pixel;
+	cam.max_depth = max_depth;
+	cam.background = color(0, 0, 0);
+	cam.vfov = 40;
+	cam.lookfrom = point3(278, 278, -800);
+	cam.lookat = point3(278, 278, 0);
+	cam.vup = vec3(0, 1, 0);
+	cam.defocus_angle = 0;
+	cam.single_thread = single_thread;
+	cam.render(world, lights);
+}
+void suzanne_mesh(int image_width, int samples_per_pixel, int max_depth, bool single_thread) {
+	hittable_list world;
+
+	auto red = make_shared<lambertian>(color(.65, .05, .05));
+	auto white = make_shared<lambertian>(color(.73, .73, .73));
+	auto green = make_shared<lambertian>(color(.12, .45, .15));
+	auto light = make_shared<diffuse_light>(color(15, 15, 15));
+
+	world.add(make_shared<quad>(point3(555, 0, 0), vec3(0, 555, 0), vec3(0, 0, 555), red));
+	world.add(make_shared<quad>(point3(0, 0, 0), vec3(0, 555, 0), vec3(0, 0, 555), green));
+	auto light_quad = make_shared<quad>(point3(343, 554, 332), vec3(-130, 0, 0), vec3(0, 0, -105), light);
+	world.add(light_quad);
+	world.add(make_shared<quad>(point3(0, 0, 0), vec3(555, 0, 0), vec3(0, 0, 555), white));
+	world.add(make_shared<quad>(point3(555, 555, 555), vec3(-555, 0, 0), vec3(0, 0, -555), white));
+	world.add(make_shared<quad>(point3(0, 0, 555), vec3(555, 0, 0), vec3(0, 555, 0), white));
+
+	shared_ptr<hittable> light_sphere
+		= make_shared<sphere>(point3(0, 0, 0), 35, make_shared<diffuse_light>(color(15, 15, 15)));
+	light_sphere = make_shared<translate>(light_sphere, vec3(470, 75, 150));
+	world.add(light_sphere);
+
+	hittable_list lights;
+	lights.add(light_quad);
+	lights.add(light_sphere);
+
+	auto mesh_mat = make_shared<lambertian>(color(0.8, 0.7, 0.6));
+	auto glass_mat = make_shared<dielectric>(1.5);
+	auto purple_metal = make_shared<metal>(color(.65, .05, .65), 0);
+	auto obj = load_obj_fit("assets/suzanne.obj", mesh_mat, 500.0, true, glass_mat, true);
+	// obj = make_shared<rotate_x>(obj, -90);
+	obj = make_shared<rotate_y>(obj, 180);
+	// obj = make_shared<rotate_z>(obj, 45);
+	obj = make_shared<translate>(obj, vec3(278, 278, 278));
 	world.add(obj);
 
 	world = hittable_list(make_shared<bvh_node>(world));
